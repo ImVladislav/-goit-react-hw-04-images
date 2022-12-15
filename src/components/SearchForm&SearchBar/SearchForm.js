7a17656964197cf250f8aea03ed92b3ea2dae3c3
ImVladislav <nafanya102@gmail.com>
2022-12-15 21:07:35 +0200

    done fix

--- src/components/SearchForm&SearchBar/SearchForm.js
+++ src/components/SearchForm&SearchBar/SearchForm.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FaSearch } from 'react-icons/fa';
 import { toast } from 'react-toastify';
